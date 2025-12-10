61bca66e71b51e01b56e9912211f2109c18a582a
patel sapna <2302031030082@silveroakuni.ac.in>
2025-12-10 10:13:42 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use(jsonServer.bodyParser);
+
+// Base URL → /api
+server.use('/api', router);
+
+server.listen(3000, () => {
+  console.log('JSON Server is running');
+});
 
